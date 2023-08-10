4eda62d62cf1c2a0c28b34786caaed2706655cc6
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-10 14:26:43 -0700

    my life

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-	mstr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char mstr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(1, mstr, 59);
 	return (1);
