f178540c15c9539a7de6fd0dc9fc61f9cbba8cc6
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-10 13:53:35 -0700

    my life

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 /**
  * main - Entry point
  *
- * prints exactly and that piece of art is useful Dora Korpar, 2015-10-19, 
+ * prints exactly and that piece of art is useful Dora Korpar, 2015-10-19,
  * followed by a new line, to the standard error.
  *
- * Return: Always 1 (not Success) 
+ * Return: Always 1 (not Success)
  *
  */
 int main(void)
 {
 	fprintf(stderr,
-			"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19 ");
 	return (1);
 }
