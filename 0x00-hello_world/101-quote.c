e2ac07307c7ede341ff22b4fdde253a5a2c533d3
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-10 13:36:43 -0700

    my life

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints exactly and that piece of art is useful Dora Korpar, 2015-10-19, 
+ * followed by a new line, to the standard error.
+ * return always success (0) 
+ *
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - 
+			Dora Korpar, 2015-10-19");
+	return (1);
+}
