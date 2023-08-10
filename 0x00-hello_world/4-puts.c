24a5676849050691b245b6685f758b5768d10b87
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-10 12:28:52 -0700

    my life

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,11 @@
  * main - Entry point
  * Return: Always 0 (Success)
  *
- * */
+*/
 int main(void)
 {
-	char[]="\"Programming is like building a multilingual puzzle,\n";
+	char mystr[] = "\"Programming is like building a multilingual puzzle,\n";
+
 	return (0);
+	puts(mystr);
 }
