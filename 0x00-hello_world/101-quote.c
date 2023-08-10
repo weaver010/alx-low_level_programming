e538c7752e40d9568b14ee1509f568fd42c64f63
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-10 14:19:12 -0700

    my life

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19"
 			);
 	return (1);
 }
