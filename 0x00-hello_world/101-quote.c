0d59ce728bffbaf55df04a85537a5e8c9958e39c
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-10 13:44:52 -0700

    my life

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
  * prints exactly and that piece of art is useful Dora Korpar, 2015-10-19, 
  * followed by a new line, to the standard error.
- * return always success (0) 
+ *
+ * Return: Always 1 (not Success) 
  *
  */
 int main(void)
