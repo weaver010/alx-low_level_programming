90f4393bc879526e1d15ef3b0c9522e2c396b2f2
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-15 10:53:12 -0700

    my life

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - print 9 time table
+ * @
+ *
+ *
+ *
+ *
+ *Return: void
+ *
+ */
+void times_table(void)
+{
+	int l_d;
+	int r;
+	int n;
+	int i;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			if ((n * i) >= 10)
+			{
+				l_d = (n * i) % 10;
+				r = (n * i) / 10;
+				_putchar(r + '0');
+				_putchar(l_d + '0');
+			}
+			else
+			{
+				_putchar((n * i) + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
