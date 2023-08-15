104d7ae8db985aba79186421dd396f763d659416
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-15 14:00:25 -0700

    my life

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -42,7 +42,7 @@ void print_times_table(int n)
 					{
 						_putchar(',');
 						_putchar(' ');
-						_putchar((i * j) + '0');
+						_putchar(' ');
 					}
 					_putchar((i * j) + '0');
 				}
