be07df3e37bfd4eb415ee8110b01d6307f04f07c
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-15 16:14:52 -0700

    my life

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ *Return: 0
+ *
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+	long i;
+	long n;
+	long sum;
+	long t;
+
+	i=1;
+	n=0;
+	while(sum < 4000000)
+	{
+		sum = i +n ;
+		t = t +sum;
+		n=i;
+		i=sum;
+	}
+	printf("%lu\n", t);
+	return (0);
+}
+
