2aaab3a0414d991ebb59a3d33add0ac13f03ee81
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-15 10:02:37 -0700

    my life

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main"
+/**
+ * _abs - return absolute value for input number
+ * @int : data type of input
+ * Return: the absolute value
+ *
+ *
+ *
+ *
+ *
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
