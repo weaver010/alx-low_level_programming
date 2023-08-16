898f3e33d533037628181366a63cefc0ea463181
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-15 17:53:56 -0700

    my life

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,59 @@
 #include <stdio.h>
+int len(int n)
+{
+	int l;
+	l=0;
+	if (n==0)
+	{
+		return(1);
+	}
+	while(n>0)
+	{
+		n=n/10;
+		l=l+1;
+	}
+	return (l);
+}
 
 int main(void)
 {
-        float n;
-        float sum;
-        float num;
-        float i;
+        int  c;
+        int  init;
+        long num1=1;
+        long num2=2;
+	long sum;
+	long max=1000000000;
+	long num3=0;
+	long num4=0;
+	long sum2=0;
 
-        i = 0;
-        num = 1;
-        for (n = 0; n < 98; n++)
+        for (c = 1; n < 99; n++)
         {
-                sum = (i + num);
-               
-               printf("%.0f", sum);
-                
-                i = num;
-                num = sum;
-		if (n !=97)
+		if (num3>0)
 		{
-			 printf(", ");
+			printf("%lu",num3);
 		}
-                
+		init=len(max)-1-len(num1);
+		while(num3>0 && init >0)
+		{
+			prnitf("%d", 0);
+			init--;
+		}
+		printf("%lu",f1);
 
-        }
-        printf("\n");
+                sum =(num1+num2)%max;
+		sum2=num3+num4+(num1+num2)/max;
+		num1=num2;
+		num3= num4;
+		num2=sum;
+		num4=sum2;
+		if(c!=98)
+		{
+			printf(",");
+		}
+		else
+		{
+			printf("\n");
+		}
         return (0);
 }
