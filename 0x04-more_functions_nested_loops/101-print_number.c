111a3d122dba06fa267fc643af2af685c46ab23b
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-17 15:49:23 -0700

    my life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_number- that prints an integer
+ *@n:the number
+ *Return:void
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
