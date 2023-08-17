057b0c5af5553bd482c4bd175a40226977e3fe43
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-17 08:17:47 -0700

    my life

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_most_numbers - prnit number except 2 4
+ *Return: void
+ *
+ *
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 50 && n != 52)
+		{
+			_putchar(n);
+		}
+	}
+}
