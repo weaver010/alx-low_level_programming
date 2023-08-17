630378c2e4694c26906c44eaa2ee685ef013218d
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-17 09:19:20 -0700

    my life

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *print_diagonal-draws a diagonal line
+ *@n:length of diagonal
+ *Return:void
+ *
+ *
+ *
+ *
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('\');
+			_putchar('\n');
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+		}
+		_Putchar('\n');
+	}
+}
