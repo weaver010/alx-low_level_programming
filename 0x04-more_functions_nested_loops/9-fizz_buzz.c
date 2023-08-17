50900ada60775de5074d56e229ffeb99f601ad15
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-17 11:01:05 -0700

    my life

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,30 +18,30 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i == 100)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
+		else if ((i % 3 != 0) && (i % 5 == 0))
+		{
+			printf("Buzz");
+		}
+		else if ((i % 3 == 0) && (i % 5 != 0))
+		{
+			printf("Fizz");
+		}
 		else
 		{
-			if ((i % 3 == 0) && (i % 5 == 0))
-			{
-				printf("FizzBuzz ");
-			}
-			else if ((i % 3 != 0) && (i % 5 == 0))
-			{
-				printf("Buzz ");
-			}
-			else if ((i % 3 == 0) && (i % 5 != 0))
-			{
-				printf("Fizz ");
-			}
-			else
-			{
-				printf("%d ", i);
-			}
+			printf("%d", i);
 		}
 	}
-	printf("\n");
+	if (n != 100)
+	{
+		printf(" ");
+	}
+	else
+	{
+		printf("\n");
+	}
 	return (0);
 }
