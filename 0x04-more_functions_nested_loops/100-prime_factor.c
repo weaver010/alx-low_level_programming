18e0fcfe0a6e756811c8b7028f8d9d9e5604d537
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-17 14:51:55 -0700

    my life

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-
-int prime(int n) 
+/**
+ *prime- ckeck if is prime
+ *@n:the number
+ *Return:1
+ *
+ *
+ *
+ *
+ */
+int prime(int n)
 {
 	int i;
-	
+
 	for (i = 2; i < n; i++)
 	{
 		if (n % i == 0)
@@ -11,9 +19,16 @@ int prime(int n)
 			return (0);
 		}
 	}
-	return(1);
+	return (1);
 }
-
+/**
+ *main - prints the largest prime factor of
+ *the number "612852475143"
+ *Return:0
+ *
+ *
+ *
+ */
 int main(void)
 {
 	long o;
@@ -25,7 +40,7 @@ int main(void)
 	n = 612852475143;
 	for (i = 2; i <= n / 10000000; i++)
 	{
-		if(prime(i))
+		if (prime(i))
 		{
 			if (o % i == 0)
 			{
@@ -33,6 +48,6 @@ int main(void)
 			}
 		}
 	}
-	printf("%d\n",d);
+	printf("%d\n", d);
 	return (0);
 }
