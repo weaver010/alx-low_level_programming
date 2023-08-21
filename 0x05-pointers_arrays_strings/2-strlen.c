332833a73fd132b676c8855216186132a4f307f4
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-21 09:05:53 -0700

    my life

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strlen -  returns the length of a string.
+ *@s: the string.
+ *Return:the length 
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+int _strlen(char *s)
+{
+	int count,i;
+	char c[] = *s;
+
+	for(i = 0; c[i] != '\0'; i++)
+	{
+		count = i;
+	}
+	return count;
+}
