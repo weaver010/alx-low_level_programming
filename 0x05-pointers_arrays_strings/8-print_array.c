fb8df209a5fb687530e7418009fba7d2fc141da6
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-21 14:11:17 -0700

    my life

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,11 +17,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
 		if (i != n - 1)
 		{
-			printf(", ");
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
 		}
 	}
-	_putchar('\n');
 }
