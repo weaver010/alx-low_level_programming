1c32872d24c520324b9d2e9c20ef8ebd6d005cf9
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-21 10:42:21 -0700

    my life

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,7 @@
  *
  *
  */
-int _strlen(char *s)
-{
-        int i;
-
-        for (i = 0; s[i] != 0; i++)
-        {
-        }
-        return (i);
-}
+int _strlen(char *s);
 void print_rev(char *s)
 {
 	int l;
