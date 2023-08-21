5b96dfc967ca15c1180791c7a40bc68ef96f7bba
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-21 16:29:56 -0700

    my life

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -17,12 +17,12 @@ int main(void)
 	char c;
 
 	srand(time(NULL));
-	while (sum <= 3000)
+	while (sum <= 2645)
 	{
 		c = rand() % 128;
 		sum += c;
 		putchar(c);
 	}
-	putchar(3127 - sum);
+	putchar(2772 - sum);
 	return (0);
 }
