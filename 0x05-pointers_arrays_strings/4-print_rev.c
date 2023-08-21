5cd501e9886572d5e850e74cc7a58cda8468aa98
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-21 11:16:59 -0700

    my life

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,33 +8,11 @@
  *
  *
  */
-
-
-/**
- *_strlen - entry point
- *@s: the string.
- *Return: the length
- *
- *
- *
- *
- *
- *
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != 0; i++)
-	{
-	}
-	return (i);
-}
 void print_rev(char *s)
 {
 	int l;
 
-	l = _strlen(s);
+	l = strlen(s);
 	for (; l > 0; l--)
 	{
 		_putchar(s[-1 + l]);
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,12 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+int strlen(char *s)
+{
+        int i;
+
+        for (i = 0; s[i] != 0; i++)
+        {
+        }
+        return (i);
+}
