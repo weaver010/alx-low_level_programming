09c785336038635672cdb9aa88cffd021ca0eb65
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-21 16:20:33 -0700

    my life

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ *main - generates random valid passwords for the program
+ *Return: 0
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(sum));
+	while (sum <= 4000)
+	{
+		c = rand();
+		sum += c;
+		putchar(c);
+	}
+	putchar(sum);
+	return (0);
+}
