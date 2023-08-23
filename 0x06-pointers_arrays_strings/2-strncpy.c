70dca8b78cb01ccc3d916e1dad2ad2c5e312f924
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-23 09:07:44 -0700

    my life

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,14 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+		if (i <= strlen(src))
+		{
+			dest[i] = src[i];
+		}
+		else
+		{
+			dest[i] = '\0';
+		}
 	}
 	return (dest);
 }
