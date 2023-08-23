cbf0ae2b5e55b2d3f16d55e94bf40fa64d0b5060
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-23 08:54:51 -0700

    my life

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * *_strncpy - Write a function that copies a string.
+ * @dest: the destnation
+ * @src:the source
+ * @n: the byte
+ *Return: dest
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
