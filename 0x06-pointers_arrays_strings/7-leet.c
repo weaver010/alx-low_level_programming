664ab4a21f025bd90db192437353d4b37762b637
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-23 17:36:15 -0700

    my life

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#includ "main.h"
+/**
+ * *leet - Write a function that encodes a
+ * string into 1337.
+ * @str: the string
+ * Return:str
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+char *leet(char *str)
+{
+	int i, t;
+	char c[] = {'4', 'a', 'A', '3', 'e',
+		'E', '0', 'O', 'o', '7', 't', 'T', '1', 'l', 'L'};
+	for (i = 0; i < istrlen(str); i++)
+	{
+		for (t = 0;  t < 15; t++)
+		{
+			if (t % 3 == 2 && str[i] == c[t]
+			&& str[i] >= 65 && str[i] <= 122)
+			{
+				str[i] = c[t - 2];
+			}
+			else if (t % 3 == 1 && str[i] == c[t]
+					&& str[i] >= 65 && str[i] <= 122)
+			{
+				str[i] = c[t - 1];
+			}
+		}
+	}
+	return (str);
+}
