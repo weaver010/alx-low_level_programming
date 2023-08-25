77028d9b54dfba593bcb52ac4274147801a2595d
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-25 02:14:59 -0700

    my life

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,5 @@
 #include"main.h"
 /**
- *sr -  returns the length of a string.
- *@s: the string.
- *Return:the length
- *
- *
- *
- *
- *
- *
- */
-int sr(char *s)
-{int i;
-
-for (i = 0; s[i] != 0; i++)
-{
-}
-return (i);
-}
-/**
  * *rot13 - function that encodes a string using rot13.
  * @str:the string
  *Return:str
@@ -34,7 +15,7 @@ char *rot13(char *str)
 	char a[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	char b[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-	for (i = 0; i < sr(str); i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (t = 0; t < sr(b); t++)
 		{
