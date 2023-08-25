480b87987bd8965e1382414446f4ab574143dfc4
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-25 00:21:25 -0700

    my life

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,15 +3,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
 void print_rev(char *s);
-int istrlen(char *s)
-{
-        int i;
-
-        for (i = 0; s[i] != 0; i++)
-        {
-        }
-        return (i);
-}
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
