b1b9f10a83de0d76d49d42d91518deaf40143e4c
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-28 12:40:33 -0700

    my life

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,11 +10,12 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, h;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (h = 0; accept[h] != s[i]; h++)
 		{
-			if ( accept[h] == '\0')
+			if (accept[h] == '\0')
 			{
 				return (i);
 			}
