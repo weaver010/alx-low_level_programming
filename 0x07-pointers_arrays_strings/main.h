9d4b7d2d26fb8d26d97e7eb66f1e91c6d20adecd
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-28 13:04:27 -0700

    my life

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -28,3 +28,6 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
