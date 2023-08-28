c945e53cc13e969a7cda74a1a5bf20543b367331
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-28 10:08:43 -0700

    my life

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -24,3 +24,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
