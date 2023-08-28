8726d47491ef19d7a877497262600d29d2036608
weaver010 <abdelrahmannagaty839@gmail.com>
2023-08-28 08:24:28 -0700

    my life

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_memcpy - function that copies memory area.
+ * @dest:destinatin
+ * @src: source
+ * @n:the size
+ *
+ *
+ *
+ *
+ *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
