e0b4de885bcf4c3ba593957977b1c5310e0f0fb7
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-01 12:36:04 -0700

    my life

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main-entry point
+ * @argc:size
+ * @argv:array
+ * Return:0
+ *
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+int i, k, c = 0;
+int a[] = {25, 10, 5, 2, 1};
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+k = atoi(argv[1]);
+if (k < 0)
+{
+printf("0\n");
+}
+for (i = 0; i < 5 && k > 0; i++)
+{
+while (k >= a[i])
+{
+k -= a[i];
+c++;
+}
+}
+printf("%d\n", c);
+return (0);
+}
