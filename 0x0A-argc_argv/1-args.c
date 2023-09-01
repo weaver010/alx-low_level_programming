6d3aef8cd194fa0cf3d68505d6a9fc57fbc2ea00
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-01 10:07:04 -0700

    my life

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,8 @@
-include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 int main(int argc, char __attribute__ ((__unused__)) **argv)
-{
-printf("%d\n", argc - 1);
+{int a;
+a=argc-1;
+printf("%d\n", a);
 return (0);
 }
