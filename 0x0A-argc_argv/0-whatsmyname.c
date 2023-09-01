d305d3c4ff207f411659603d086f8fef80e57ffa
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-01 10:21:43 -0700

    my life

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * Return:0
  *
  */
-int main(int __attribute__ ((unused)) argc, char **argv)
+int main(int __attribute__ ((unused)) argc, char *argv[])
 {
-printf("%s \n", argv[0]);
+printf("%s \n", *argv);
 return (0);
 }
