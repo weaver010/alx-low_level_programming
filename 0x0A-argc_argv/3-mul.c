4ca69e27cad3bea20f247e44948c5f1dd307fdc7
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-01 10:34:43 -0700

    my life

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main-entry point
+ * @argc:size
+ * @argv:array
+ * Return:0
+ *
+ */
+int main(int argc, char* argv[])
+{
+int i,m = 1;
+for (i = 1; i < argc; i++)
+{
+m *= atoi(argv[i]);
+}
+if (argc == 2)
+{	
+printf("%d", m);
+}
+else
+{
+printf("Error");
+}
+return (0);
+}
