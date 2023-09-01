4f8353b9c6efbe9d38646770dcaac0a19841388d
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-01 10:50:43 -0700

    my life

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,34 @@
 #include"main.h"
 #include<stdio.h>
-#include<stdlib.h>
+/**
+ *_atoi - convert a string to an integer.
+ *@s:the string
+ *Return:intger
+ *
+ *
+ *
+ */
+int _atoi(char *s)
+{
+unsigned int n = 0;
+int si = 1;
+do {
+if (*s == '-')
+{
+si *= -1;
+}
+else if (*s >= '0' && *s <= '9')
+{
+n = (n * 10) + (*s - '0');
+}
+else if (n > 0)
+{
+break;
+}
+} while (*s++);
+return (n *si);
+}
+
 /**
  * main-entry point
  * @argc:size
@@ -8,15 +36,15 @@
  * Return:0
  *
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-int i,m = 1;
+int i, m = 1;
 for (i = 1; i < argc; i++)
 {
-m *= atoi(argv[i]);
+m *= _atoi(argv[i]);
 }
 if (argc == 2)
-{	
+{
 printf("%d", m);
 }
 else
