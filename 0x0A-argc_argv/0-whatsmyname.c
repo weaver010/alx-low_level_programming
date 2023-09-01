3d50aca9c17a9c59ac3c1802adaedd7b232540d4
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-01 09:36:11 -0700

    my life

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main-entry point
+ * @argc:size
+ * @argv:array
+ * Return:0
+ *
+ */
+int main(int __attribute__ ((unused)) argc, char *argv)
+{
+printf("%s \n", argv[0]);
+return (0);
+}
