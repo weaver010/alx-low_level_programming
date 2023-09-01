52c83dfb20bdb183c5b5aaabba843c6e6fcee234
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-01 10:29:46 -0700

    my life

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main-entry point
+ * @argc:size
+ * @argv:array
+ * Return:0
+ *
+ */
+int main(int __attribute__ ((unused))argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
