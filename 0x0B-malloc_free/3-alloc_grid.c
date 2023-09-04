21cd8842d2803096224aadae4f6a4d1fcbc3e438
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-04 12:44:11 -0700

    my life

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include<stdlib.h>
+/**
+ * **alloc_grid- prints a grid of integers
+ * @width: width
+ * @height: height
+ * Return: a
+ */
+int **alloc_grid(int width, int height)
+{int k, i, o;
+int **a;
+a = malloc(height * sizeof(int));
+if (width <= 0 || height <= 0 || a == 0)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{a[i] = malloc(width * sizeof(int));
+if (a[i] == 0)
+{
+for (o = i; o > 0; o--)
+{
+free(a[i]);
+}
+free(a);
+return (NULL);
+}
+}
+for (i = 0; i < height; i++)
+{
+for (k = 0; k < width; k++)
+{
+a[i][k] = 0;
+}
+}
+return (a);
+}
