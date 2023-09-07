f9d10e72c9dc2af711792098903e06eb5557bd50
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-07 09:07:03 -0700

    my life

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,9 +10,13 @@
  */
 void *malloc_checked(unsigned int b)
 {
-int *s = malloc(b);
+void *s = malloc(b);
 if (s == NULL)
-{exit(98);
+{
+exit(98);
 }
-return (s);
+else
+{
+return (malloc(b));
+}
 }
