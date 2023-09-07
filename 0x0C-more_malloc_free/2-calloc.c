f97740fc73e31e36ebf53ed037ad6748225adf98
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-07 12:10:09 -0700

    my life

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,23 +4,23 @@
  *  @nmemb:the num of element
  *  @size:the size
  *  Return:s
- * 
+ *
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {char *s;
 size_t i;
-if(nmemb == 0 || size == 0)
+if (nmemb == 0 || size == 0)
 {
 return (NULL);
 }
 s = malloc(nmemb * size);
-if(s == NULL)
+if (s == NULL)
 {
 return (NULL);
 }
-for(i = 0; i < nmemb * size; i++)
+for (i = 0; i < (nmemb * size); i++)
 {
-    s[i] = 0;
+s[i] = 0;
 }
 return (s);
 }
