62e23ad984332160ef06c8315c20cdbf2002f7c3
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-07 11:06:04 -0700

    my life

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,10 +38,11 @@ for (i = 0; i < strlen(s1); i++)
 {
 s[i] = s1[i];
 }
-for (j = strlen(s1); j <= strlen(s1) + n+1; j++)
+for (j = strlen(s1); j < strlen(s1) + n; j++)
 {
 s[j] = s2[o];
 o++;
 }
+s[j] = '\0';
 return (s);
 }
