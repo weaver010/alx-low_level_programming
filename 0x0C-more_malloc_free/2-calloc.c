1e435e1cd821f639bbe5f630c22ace6852fc792c
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-07 12:07:35 -0700

    my life

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,18 +9,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {char *s;
 size_t i;
-if(nmemb==0||size==0)
+if(nmemb == 0 || size == 0)
 {
-return(NULL);
+return (NULL);
 }
-s=malloc(nmemb*size);
-if(s==NULL)
+s = malloc(nmemb * size);
+if(s == NULL)
 {
-return(NULL);
+return (NULL);
 }
-for(i=0;i<nmemb*size;i++)
+for(i = 0; i < nmemb * size; i++)
 {
-    s[i]=0;
+    s[i] = 0;
 }
-return(s);
+return (s);
 }
