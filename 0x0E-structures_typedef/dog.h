9e210f275ffa84acd1daa8b3328e9186b96e663c
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-11 09:42:47 -0700

    my life

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - Short description
  * @name: First member
@@ -12,3 +14,4 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
