6dff93006d190afc6391473fd637cdaf8dcc1931
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-11 12:50:19 -0700

    my life

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,13 +19,6 @@ printf("Name: (nil)\n");
 else
 {
 printf("Name: %s\n", d->name);
-}
-if (d->age < 0)
-{
-printf("Age: (nil)\n");
-}
-else
-{
 printf("Age: %f\n", d->age);
 }
 if (d->owner == NULL)
