4f05f030ca2e47e097a1bdbb37ca51b73d9ed125
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-11 11:00:20 -0700

    my life

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void print_dog(struct dog *d)
+{
+if(d!=NULL)
+{
+if(d->name==NULL)
+{
+printf("Name: (nil)\n");
+}
+else
+{
+printf("Name: %s\n",d->name);
+}
+if (!(d->age))
+{
+printf("age: (nil)\n");
+}         
+else
+{
+printf("age: %f\n",d->age);
+}
+if(d->owner==NULL)
+{
+printf("owner: (nil)\n");
+}
+else
+{
+printf("owner: %s",d->owner);
+}
+}
+}
