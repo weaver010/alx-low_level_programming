1e37a47a92934cdf28a19d99b38983c297862b7f
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-13 11:49:20 -0700

    my life

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef f_h
-#define f_h
+#ifndef function_pointers_h
+#define function_pointers_h
 #include<stdlib.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
