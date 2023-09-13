83fc718b9bddfb9bbd745fec6496e5636434cba0
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-13 14:57:35 -0700

    my life

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry
+ * @argc:the size
+ * @argv:the array
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{int b, i;
+char *s;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+b = atoi(argv[1]);
+if (b < 0)
+{
+printf("Error\n");
+exit(2);
+}
+s = (char *)main;
+for (i = 0; i < b; i++)
+{
+if (i == b--)
+{
+printf("%02hhx\n", s[i]);
+break;
+}
+printf("%02hhx ", s[i]);
+}
+return (0);
+}
