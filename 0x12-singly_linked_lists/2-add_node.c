a4f83537fc7810885f5f465a9e50980767309b6c
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-21 12:44:28 -0700

    my life

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,9 +1,9 @@
 #include"lists.h"
 /**
- *
- *
- *
- *
+ * *add_node - add node
+ * @head:the head
+ *@str:the str
+ *Return:address
  *
  *
  *
@@ -12,11 +12,14 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *s;
-s=malloc(sizeof(list_t));
-s->str=strdup(str);
-s->len=strlen(str);
-s->next=*head;
-*head=s;
-return(s);
-    
+s = malloc(sizeof(list_t));
+if (s == NULL)
+{
+return (NULL);
+}
+s->str = strdup(str);
+s->len = strlen(str);
+s->next = *head;
+*head = s;
+return (s);
 }
