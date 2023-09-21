db962d5a494fa1a5d14eea75a8d8d25789615fbc
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-21 12:32:50 -0700

    my life

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,24 @@
 #include"lists.h"
+/**
+ * print_list - print list
+ * @h:the node
+ * Return:the num of node
+ */
 size_t print_list(const list_t *h)
 {
-int o=0;
-while(h)
+int o = 0;
+while (h)
 {
-if(h->str==NULL)
+if (h->str == NULL)
 {
 printf("[0] (nil)\n");
 }
 else
 {
-printf("[%d] %s\n",h->len,h->str);
+printf("[%d] %s\n", h->len, h->str);
 }
-h=h->next;
-
+h = h->next;
 o++;
 }
-return(o);
+return (o);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,16 @@
 #include"lists.h"
+/**
+ * list_len - return len
+ * @h:the node
+ *Return:o
+ *
+ */
 size_t list_len(const list_t *h)
-{int o= 0;
-while(h)
+{int o = 0;
+while (h)
 {
-
-h=h->next;
+h = h->next;
 o++;
 }
-return(o);
+return (o);
 }
