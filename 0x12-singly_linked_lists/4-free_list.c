705113fa765fc9e0ba2e8a198195dad9b24ac8d1
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-21 13:55:02 -0700

    my life

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,12 +1,19 @@
 #include"lists.h"
+/**
+ *free_list -free list
+ *@head:the node
+ *Rrturn:void
+ *
+ *
+ */
 void free_list(list_t *head)
-{  list_t *s;
-
-    while(head)
 {
-    s=head->next;
-    free(head->str);
-    free(head);
-    head=s;
+list_t *s;
+while (head)
+{
+s = head->next;
+free(head->str);
+free(head);
+head = s;
 }
 }
