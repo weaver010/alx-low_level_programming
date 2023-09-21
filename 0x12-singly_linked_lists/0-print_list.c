ee475253bd87ffb0fa2df451f52795cc506b5915
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-21 10:29:00 -0700

    my life

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,20 +2,16 @@
 size_t print_list(const list_t *h)
 {
 int o=0;
-list_t *copy;
-copy=malloc(sizeof(list_t));
-copy->str=h->str;
-copy->len=h->len;
-copy->next=h->next;
-while(copy)
+while(h)
 {
-if(copy->str==NULL)
+if(h->str==NULL)
 {
-copy->str="(nil)";
-copy->len=0;
 }
-printf("[%d] %s\n",copy->len,copy->str);
-copy=copy->next;
+else
+{
+printf("[%d] %s\n",h->len,h->str);
+h=h->next;
+}
 o++;
 }
 return(o);
