3c7a493a681dd0b7e3bc3e44cab9780259e0c832
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-21 12:48:52 -0700

    my life

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,27 +1,34 @@
 #include"lists.h"
+/**
+ * *add_node_end - add node
+ * @head:the head
+ *@str:the str
+ *Return:address
+ *
+ *
+ *
+ *
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *s,*t=*head;
-s=malloc(sizeof(list_t));
-s->str=strdup(str);
-s->len=strlen(str);
-s->next=NULL;
-
-
+list_t *s, *t = *head;
+s = malloc(sizeof(list_t));
+if (s == NULL)
+{
+return (NULL);
+}
+s->str = strdup(str);
+s->len = strlen(str);
+s->next = NULL;
 if (*head == NULL)
 {
-		*head = s;
-		return (s);
+*head = s;
+return (s);
 }
-
-
-while(t->next)
+while (t->next)
 {
-    t=t->next;
+t = t->next;
 }
-t->next=s;
-
-
-return(s);
-
+t->next = s;
+return (s);
 }
