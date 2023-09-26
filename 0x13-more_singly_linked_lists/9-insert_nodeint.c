ea1403a2d3b13bef44306ffd271ace875e49ffb0
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-26 12:37:32 -0700

    my life

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include"lists.h"
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *c;
+	listint_t *cop = *head;
+
+	c = malloc(sizeof(listint_t));
+	if (c == NULL)
+		{return (NULL);}
+		if (head == NULL)
+		{return (NULL);}
+
+	c->n = n;
+	c->next = NULL;
+
+	if (idx == 0)
+	{
+		c->next = *head;
+		*head = c;
+		return (c);
+	}
+
+	while (  i < idx)
+	{
+	 if(!cop)
+	{
+	    break;
+	}
+		if (i == idx - 1)
+		{
+			c->next = cop->next;
+			cop->next = c;
+			return (c);
+		}
+		else
+			cop = cop->next;
+			i++;
+	}
+
+	return (NULL);
+}
