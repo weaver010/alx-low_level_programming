c70b44685dc2ddb6f4badc65a67d8537b8c7c2a1
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-26 11:56:37 -0700

    my life

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,15 @@
+#include"lists.h"
+int pop_listint(listint_t **head)
+{
+	listint_t *c;
+	int dat;
+if (head == NULL)
+{
+return (0);
+}
+dat = (*head)->n;
+c = (*head)->next;
+free(*head);
+*head = c;
+return (dat);
+}
