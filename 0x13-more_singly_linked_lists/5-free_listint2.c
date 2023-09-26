ec484cdd65eff7c36aadf0d96a1c759920841b07
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-26 11:27:10 -0700

    my life

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include"lists.h"
+/**
+ *free_listint2 -free list
+ *@head:the node
+ *Rrturn:void
+ *
+ *
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *c;
+while (*head)
+{
+c = *head->next;
+free(*head);
+*head = c;
+}
+*head = NULL;
+}
