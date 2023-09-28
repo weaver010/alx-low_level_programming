b5e957a010265375779903ba5d3ea89371972535
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-28 12:04:46 -0700

    my life

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include"main.h"
+void o(unsigned long int c)
+{
+if (c>0)
+{
+   o(c/2);
+  _putchar((((c&1) )+'0'));
+}
+
+}
+void print_binary(unsigned long int n)
+{unsigned long int c=n;
+
+o(c);
+
+
+if(c==0)
+{
+    _putchar('0');
+}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <math.h>
 int _putchar(char c);
+void o(unsigned long int c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
