78cf3a3a70e6c800fda11da06a4880ca287b85d6
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-28 09:35:37 -0700

    my life

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,18 @@
 #include"main.h"
 unsigned int binary_to_uint(const char *b)
 {
-char *c=malloc(sizeof(char)*strlen(b));
  unsigned int sum =0,i;
- strcpy(c,b);
  if(b==NULL)
  {
      return(0);
  }
  else
  {
-for( i =0;c[i]!='\0';i++)
+for( i =0;b[i]!='\0';i++)
 {
- if (c[i]==48||c[i]==49)
+ if (b[i]==48||b[i]==49)
     {
-    sum+=((c[strlen(c)-1-i]-48)*pow(2,i));
+    sum+=((b[strlen(b)-1-i]-48)*pow(2,i));
 
     }
     else
