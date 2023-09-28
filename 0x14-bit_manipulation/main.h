fad74004a95c40503f1725593dd9b32864f2d70c
weaver010 <abdelrahmannagaty839@gmail.com>
2023-09-28 14:22:14 -0700

    my life

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@
 #include <math.h>
 int _putchar(char c);
 void o(unsigned long int c);
+int get_endianness(void);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
