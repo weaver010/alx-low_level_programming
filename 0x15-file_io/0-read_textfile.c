e0d8c989c6b686a8b3826b49d011ce799a0ffb9e
weaver010 <abdelrahmannagaty839@gmail.com>
2023-10-02 10:16:24 -0700

    my life

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,20 +1,23 @@
-#include "main.h"
+#include"main.h"
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
-
-	free(buf);
-	close(fd);
-	return (w);
+char *ter;
+ssize_t f;
+ssize_t w;
+ssize_t t;
+if (filename || letters)
+{
+return (0);
 }
-
+f = open(filename, O_RDONLY);
+if (f == -1)
+{
+return (0);
+}
+buf = malloc(sizeof(char) * letters);
+t = read(f, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
+free(buf);
+close(f);
+return (w);
+}
