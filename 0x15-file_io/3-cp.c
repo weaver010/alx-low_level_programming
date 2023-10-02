93d44f1da9f9a7066c8bed9d8d32312c7ab21e13
weaver010 <abdelrahmannagaty839@gmail.com>
2023-10-02 12:31:52 -0700

    my life

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,51 +1,50 @@
 #include"main.h"
+/**
+ * main -entry
+ * @argc:the size
+ * @argv:the iput
+ * Return:o
+ */
 int main(int argc, char *argv[])
 {
-int sour, dest, s, o,c,b;
+int sour, dest, s, o, c, b;
 char *buf;
 buf = malloc(sizeof(char) * 1024);
 if (buf == NULL)
 {
-dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99); }
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
+exit(97); }
 sour = open(argv[1], O_RDONLY);
 s = read(sour, buf, 1024);
 dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 do {
 if (sour == -1 || s == -1)
 {
-dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 free(buf);
-exit(98);
-}
+exit(98); }
 o = write(dest, buf, s);
 if (dest == -1 || o == -1)
 {
-dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 free(buf);
-exit(99);
-}
+exit(99); }
 s = read(sour, buf, 1024);
 dest = open(argv[2], O_WRONLY | O_APPEND);
 } while (s > 0);
 free(buf);
 c = close(sour);
 b = close(dest);
-if (c == -1 )
+if (c == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", sour);
-exit(100);
-}
+exit(100); }
 if (b == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest);
-exit(100);
-}
-return (0);
-}
+exit(100); }
+return (0); }
