d42f703e753b40f8c85f2245a209a436f5c8c5ca
weaver010 <abdelrahmannagaty839@gmail.com>
2023-10-02 10:46:55 -0700

    my life

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@ if (filename == NULL)
 {
 return (-1);
 }
-f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 o = write(f, text_content, strlen(text_content));
 if (f == -1 || o == -1)
 return (-1);
