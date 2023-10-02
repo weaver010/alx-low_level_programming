3e0d6fdfeab7d64ede5d0636bc5ada27609817db
weaver010 <abdelrahmannagaty839@gmail.com>
2023-10-02 10:42:04 -0700

    my life

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,15 @@
+#include"main.h"
+int create_file(const char *filename, char *text_content)
+{
+int f, o;
+if (filename == NULL)
+{
+return (-1);
+}
+f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+o = write(f, text_content, strlen(text_content));
+if (f == -1 || o == -1)
+return (-1);
+close(f);
+return (1);
+}
