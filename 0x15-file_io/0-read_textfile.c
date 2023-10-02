4f8e041e653aee3fe31f8e5fb67e7d7a2908ef88
weaver010 <abdelrahmannagaty839@gmail.com>
2023-10-02 09:51:27 -0700

    my life

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,17 @@
+#include "main.h"
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *ter;
+	ssize_t f;
+	ssize_t o;
+	ssize_t s;
+
+	f = open(filename, O_RDONLY);
+	if (f == -1)
+	return (0);
+	ter = malloc(letters*sizeof(char));
+	s = read(f, &ter[0], letters);
+	o = write(STDOUT_FILENO, &ter[0], s);
+	return (o);
+}
+
