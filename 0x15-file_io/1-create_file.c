900e3c61c0b39dc947aad51e7e61690f457fe6ae
weaver010 <abdelrahmannagaty839@gmail.com>
2023-10-02 11:13:56 -0700

    my life

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,16 @@
 #include"main.h"
+int slen(char *s)
+{
+int i=0;
+if (s != NULL)
+{
+for (i = 0; s[i];)
+{
+i++;
+}
+}
+return(i);
+}
 int create_file(const char *filename, char *text_content)
 {
 int f, o;
@@ -7,7 +19,7 @@ if (filename == NULL )
 return (-1);
 }
 f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-o = write(f, text_content, strlen(text_content));
+o = write(f, text_content, slen(text_content));
 if (f == -1 || o == -1)
 {
 return (-1);
