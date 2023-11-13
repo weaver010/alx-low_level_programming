cb45b7d149e1d11dc230f2f5b0cd98ae02e90ecd
weaver010 <abdelrahmannagaty839@gmail.com>
2023-11-13 12:48:20 -0800

    hi

---
+++ 0x18-dynamic_libraries/g.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int add(int a,int b)
+{
+	return a+b;
+}
+int sub(int a, int b)
+{
+	return a-b;
+}
+int mul(int a, int b)
+{
+return a*b;
+}
+int div(int a, int b)
+{
+	if (b==0)
+	{
+		printf("error");
+	}
+	else
+	{
+	return a/b;
+	}
+}
+int mod(int a, int b)
+{
+        if (b==0)
+        {
+                printf("error");
+        }
+        else
+        {
+        return a%b;
+        }
+}
