ea02cebb5d72d84ff261572936dc208d96a75539
weaver010 <abdelrahmannagaty839@gmail.com>
2023-12-01 09:15:44 -0800

    my life

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ * @ht:hash table
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *s;
+	unsigned long int i = 0, o = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+
+	for (i = o; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			s = ht->array[i];
+			while (s)
+			{
+			    if (o == 0)
+			    {
+				    printf("%s'%s': '%s'", "", s->key, s->value), o++;
+			    }
+			    else
+			    {
+			        printf("%s'%s': '%s'", ", ", s->key, s->value), o++;
+			    }
+				s = s->next;
+			}
+		}
+	}
+	printf("}\n");
+}
