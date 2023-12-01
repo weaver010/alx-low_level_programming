a861627aaf15b6a444173a8b6c5facc4e06a8da4
weaver010 <abdelrahmannagaty839@gmail.com>
2023-12-01 08:52:32 -0800

    my life

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get -  retrieves a value associated with a key.
+ * @ht:the hash table.
+ * @key:key
+ * Return:s_value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *s;
+	unsigned long int i;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		return (NULL);
+    }
+	i = key_index((const unsigned char *)key, ht->size);
+	if (i >= ht->size)
+	{
+		return (NULL);
+	}
+	s = ht->array[i];
+	while (s && strcmp(s->key, key) != 0)
+	{
+		s = s->next;
+	}
+	if(s == NULL) 
+	{
+	    return (NULL);
+	}
+	else
+	{
+	    return (s->value)
+	}
+}
