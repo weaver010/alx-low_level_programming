f4f21d8a7f708cbe7bf0ba2a214c30fe1860acb5
weaver010 <abdelrahmannagaty839@gmail.com>
2023-12-01 07:55:07 -0800

    hi

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Creates  hash table.
+ * @size:size
+ *
+ * Return: ht
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_t;
+	size_t i;
+
+	hash_t = malloc(sizeof(hash_table_t));
+	if (hash_t == NULL)
+	{
+		return (NULL);
+	}
+	hash_t->size = size;
+	hash_t->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_t->array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		hash_t->array[i] = NULL;
+	}
+	return (hash_t);
+}
